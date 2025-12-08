434e5015309c3b932cfd05a7ea15a37b9a05b3ea
Megha <meghaninave8@gmail.com>
2025-12-08 10:04:48 +0530

    8/12/25

--- module/http/app.js
+++ module/http/app.js
@@ -1,17 +1,17 @@
 import http from "http";
-import fs from "fs";
+import fs from 'fs'
+const server=http.createServer((req,res)=>{
+   
+   
+const date=new Date();
+ const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
 
-const server = http.createServer((req, res) => {
-  const date = new Date();
-  const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()} ${date.toLocaleTimeString()}`;
+    res.writeHead(200,{"content-type":"text/plain"});
+    fs.writeFileSync("logger.txt",`Request received at: ${formattedDate}\n`);
+    fs.appendFileSync("logger.txt",`\n A reuest comes on server at: ${formattedDate}\n`);
 
-  res.writeHead(200, { "Content-Type": "text/plain" });
-
-  fs.appendFileSync("logger.txt", `Request received at: ${formattedDate}\n`);
-
-  res.end("Hello World");
-});
-
-server.listen(4540, () => {
-  console.log("server started");
-});
+   res.end("hello world"); 
+})
+server.listen(4540,()=>{
+    console.log("server started");
+})
